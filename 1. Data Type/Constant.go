25682caf413f755d00f4b40a167de02d79dad931
Alexander <romian37@gmaiil.com>
2025-11-21 12:26:09 +0700

    1. Data Type

---
+++ 1. Data Type/Constant.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	const firstName string = "Alex"
+	const lastName = "S"
+
+	//error
+	// firstName = "Alexander"
+
+	// multiple const
+	const (
+		namaDepan    string = "Alex"
+		namaBelakang        = "S"
+	)
+}
